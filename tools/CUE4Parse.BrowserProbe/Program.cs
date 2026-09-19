5a0ce7a936d17814106ca121858cdbe21eeec501
E8uc <313141439+E8uc@users.noreply.github.com>
2026-09-19 14:37:41 -0400

    Add CUE4Parse browser-WASM probe entry point

---
+++ tools/CUE4Parse.BrowserProbe/Program.cs
@@ -0,0 +1,6 @@
+using CUE4Parse.UE4.IO;
+using CUE4Parse.UE4.IO.Objects;
+
+Console.WriteLine("NovaSparx CUE4Parse browser probe");
+Console.WriteLine(typeof(IoStoreReader).FullName);
+Console.WriteLine(typeof(FIoStoreTocResource).FullName);
