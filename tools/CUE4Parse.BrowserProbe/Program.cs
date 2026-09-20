0430fd8f5e78d534dbb9586744501137b9580673
E8uc <313141439+E8uc@users.noreply.github.com>
2026-09-20 04:19:35 -0400

    Make CUE4Parse browser probe runtime-verifiable

--- tools/CUE4Parse.BrowserProbe/Program.cs
+++ tools/CUE4Parse.BrowserProbe/Program.cs
@@ -2,5 +2,16 @@ using CUE4Parse.UE4.IO;
 using CUE4Parse.UE4.IO.Objects;
 
 Console.WriteLine("NovaSparx CUE4Parse browser probe");
+
+if (!OperatingSystem.IsBrowser())
+{
+    Console.Error.WriteLine("Probe is not running on the browser-wasm runtime.");
+    return 2;
+}
+
+Console.WriteLine(typeof(IoStoreReader).Assembly.FullName);
 Console.WriteLine(typeof(IoStoreReader).FullName);
 Console.WriteLine(typeof(FIoStoreTocResource).FullName);
+Console.WriteLine("CUE4PARSE_BROWSER_WASM_OK");
+
+return 0;
